87904faee416744563b22802b49cce2668407bb1
RB <xtheme@gmail.com>
2021-04-09 10:19:14 +0800

    feat: 漫畫模型與控制器

---
+++ app/Http/Controllers/Backend/BookCategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\BookCategory;
+use Illuminate\Http\Request;
+
+class BookCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response('backend/book_category');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BookCategory  $bookCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BookCategory $bookCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BookCategory  $bookCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BookCategory $bookCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BookCategory  $bookCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BookCategory $bookCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BookCategory  $bookCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BookCategory $bookCategory)
+    {
+        //
+    }
+}
