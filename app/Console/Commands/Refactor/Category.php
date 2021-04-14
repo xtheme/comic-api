ac39b4aa6be98c8898e3197a4fa1f3ccddd8bf35
RB <xtheme@gmail.com>
2021-04-14 16:13:05 +0800

    feat: 分類轉標籤腳本

---
+++ app/Console/Commands/Refactor/Category.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Console\Commands\Refactor;
+
+use App\Models\Book;
+use App\Models\BookCategory;
+use App\Models\User;
+use Conner\Tagging\Model\Tag;
+use Conner\Tagging\Model\TagGroup;
+use Illuminate\Console\Command;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class Category extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'refactor:categories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '重構 category 資料表, 將數據轉換到 tagging_tags, 漫畫類別改用關聯方式查詢, 执行且请先备份数据表!';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if ($this->confirm('请确认是否运行重构脚本? 执行且请先备份数据表!')) {
+            // 標籤分組
+            // $groups = ['category', 'section'];
+            //
+            // foreach ($groups as $group) {
+            //     TagGroup::create(['name' => $group]);
+            // }
+
+            // $categories = BookCategory::all();
+            //
+            // $bulk = $categories->reject(function ($item) {
+            //     return $item->name === '';
+            // })->map(function ($item) {
+            //     return [
+            //         // 'slug' => mb_strtolower(pinyin_permalink($item->name), 'UTF-8'),
+            //         'slug' =>  mb_strtolower($item->name, 'UTF-8'),
+            //         'name' => $item->name,
+            //         'suggest' => $item->status,
+            //         'count' => 0,
+            //         'tag_group_id' => 1,
+            //         'description' => $item->desc,
+            //         'queries' => 0,
+            //         'priority' => $item->orders,
+            //     ];
+            // })->toArray();
+            //
+            // // 转移类别数据到 tagging_tags
+            // DB::table('tagging_tags')->insert($bulk);
+            //
+            // $this->line('category 數據已轉移到 tagging_tags');
+
+            $books = Book::all();
+
+            $books->each(function ($book) {
+                $ids = explode(',', $book->cate_id);
+                $categories = BookCategory::where('status', 1)->whereIn('id', $ids)->get();
+                $categories = $categories->reject(function ($category) {
+                    return Tag::where('name', $category->name)->exists() === false;
+                })->map(function ($category) {
+                    return $category->name;
+                })->toArray();
+
+                $book->tag($categories);
+            });
+
+            $this->line('漫畫類別已改用標籤關聯');
+        }
+
+        return 0;
+    }
+}
--- app/Console/Commands/Refactor/Users.php
+++ app/Console/Commands/Refactor/Users.php
@@ -22,7 +22,7 @@ class Users extends Command
      *
      * @var string
      */
-    protected $description = '重構 users 資料表結構';
+    protected $description = '重构 users 数据表结构, 执行且请先备份数据表!';
 
     protected $table = 'users_clone';
 
@@ -43,96 +43,97 @@ class Users extends Command
      */
     public function handle()
     {
-        Schema::table('users_clone', function (Blueprint $table) {
-            // 移除索引
-            $table->dropIndex('openid');
-            $table->dropIndex('gzopenid');
-            $table->dropIndex('payopenid');
-            $table->dropIndex('tgid');
-            $table->dropIndex('did');
-            $table->dropIndex('sxid');
+        if ($this->confirm('请确认是否运行重构脚本? 执行且请先备份数据表!')) {
+            Schema::table('users_clone', function (Blueprint $table) {
+                // 移除索引
+                $table->dropIndex('openid');
+                $table->dropIndex('gzopenid');
+                $table->dropIndex('payopenid');
+                $table->dropIndex('tgid');
+                $table->dropIndex('did');
+                $table->dropIndex('sxid');
 
-            // 移除欄位
-            $columns = [
-                'name',
-                'nickname',
-                'password',
-                'email',
-                'email_bind',
-                'avatar',
-                'money',
-                'role',
-                'group',
-                'sort',
-                'openid',
-                'did',
-                'isvip',
-                'vipstime',
-                'vipetime',
-                'fcbl',
-                'xingming',
-                'fangshi',
-                'zhanghao',
-                'tgid',
-                'guanzhu',
-                'ewm',
-                'isguanzhu',
-                'sxid',
-                'isout',
-                'gzopenid',
-                'agentlogin',
-                'payopenid',
-                'zjgz',
-                'idnumber',
-                'qq_id',
-                'qq_uid',
-                'weibo_id',
-                'weixin_uid',
-                'weixin_id',
-                'auto_buy',
-                'user_type',
-                'tzurl',
-                'device_tokens',
-                'cover_img',
-                'invite_uid',
-                'invite_install_code',
-                'version_type',
-            ];
-            $table->dropColumn($columns);
+                // 移除欄位
+                $columns = [
+                    'name',
+                    'nickname',
+                    'password',
+                    'email',
+                    'email_bind',
+                    'avatar',
+                    'money',
+                    'role',
+                    'group',
+                    'sort',
+                    'openid',
+                    'did',
+                    'isvip',
+                    'vipstime',
+                    'vipetime',
+                    'fcbl',
+                    'xingming',
+                    'fangshi',
+                    'zhanghao',
+                    'tgid',
+                    'guanzhu',
+                    'ewm',
+                    'isguanzhu',
+                    'sxid',
+                    'isout',
+                    'gzopenid',
+                    'agentlogin',
+                    'payopenid',
+                    'zjgz',
+                    'idnumber',
+                    'qq_id',
+                    'qq_uid',
+                    'weibo_id',
+                    'weixin_uid',
+                    'weixin_id',
+                    'auto_buy',
+                    'user_type',
+                    'tzurl',
+                    'device_tokens',
+                    'cover_img',
+                    'invite_uid',
+                    'invite_install_code',
+                    'version_type',
+                ];
+                $table->dropColumn($columns);
 
-            // 加上時間戳
-            $table->timestamps();
+                // 加上時間戳
+                $table->timestamps();
 
-            $table->timestamp('last_login_at')->after('last_login_time')->nullable();
-        });
-        $this->line('数据表结构已重构');
+                $table->timestamp('last_login_at')->after('last_login_time')->nullable();
+            });
+            $this->line('数据表结构已重构');
 
-        DB::statement(sprintf('ALTER TABLE %s CHANGE `subscribed_at` `subscribed_at` TIMESTAMP NULL DEFAULT NULL', DB::getTablePrefix() . $this->table));
-        $this->line('subscribed_at 属性已变更为 timestamp');
+            DB::statement(sprintf('ALTER TABLE %s CHANGE `subscribed_at` `subscribed_at` TIMESTAMP NULL DEFAULT NULL', DB::getTablePrefix() . $this->table));
+            $this->line('subscribed_at 属性已变更为 timestamp');
 
-        DB::table($this->table)->whereNull('created_at')->update(['created_at' => DB::raw('FROM_UNIXTIME(`create_time`)')]);
-        $this->line('created_at 数据已填充');
+            DB::table($this->table)->whereNull('created_at')->update(['created_at' => DB::raw('FROM_UNIXTIME(`create_time`)')]);
+            $this->line('created_at 数据已填充');
 
-        DB::table($this->table)->whereNull('updated_at')->where('update_time', '!=', 0)->update(['updated_at' => DB::raw('FROM_UNIXTIME(`update_time`)')]);
-        $this->line('updated_at 数据已填充');
+            DB::table($this->table)->whereNull('updated_at')->where('update_time', '!=', 0)->update(['updated_at' => DB::raw('FROM_UNIXTIME(`update_time`)')]);
+            $this->line('updated_at 数据已填充');
 
-        DB::table($this->table)->whereNull('last_login_at')->where('last_login_time', '!=', 0)->update(['last_login_at' => DB::raw('FROM_UNIXTIME(`last_login_time`)')]);
-        $this->line('last_login_at 数据已填充');
+            DB::table($this->table)->whereNull('last_login_at')->where('last_login_time', '!=', 0)->update(['last_login_at' => DB::raw('FROM_UNIXTIME(`last_login_time`)')]);
+            $this->line('last_login_at 数据已填充');
 
-        Schema::table('users_clone', function (Blueprint $table) {
-            // 移除欄位
-            $columns = [
-                'last_login_time',
-                'create_time',
-                'update_time',
-            ];
-            $table->dropColumn($columns);
-            $this->line('重复字段已排除');
+            Schema::table('users_clone', function (Blueprint $table) {
+                // 移除欄位
+                $columns = [
+                    'last_login_time',
+                    'create_time',
+                    'update_time',
+                ];
+                $table->dropColumn($columns);
+                $this->line('重复字段已排除');
+                // rename
+            });
 
-            // rename
-        });
-
-        $this->line('数据表结构已重构');
+            $this->line('数据表结构已重构');
+        }
 
         return 0;
     }
