5bea141d6a5daca86a5fe89a5beab19dcb1e6bc4
max91246 <max91246@gmail.com>
2021-04-19 17:58:08 +0800

    feat: 舉報 view add

---
+++ resources/views/backend/bookreporttype/edit.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.modal')
+
+{{-- page style --}}
+@section('page-styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendors/css/pickers/daterange/daterangepicker.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/plugins/forms/validation/form-validation.css') }}">
+@endsection
+
+@section('content')
+    <form id="form" class="form" method="post" action="{{ route('backend.bookreporttype.update', $data->id) }}" novalidate>
+        @method('put')
+        <div class="form-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="form-group">
+                        <label for="input-username"><span class="danger">*</span>举报问题名称</label>
+                        <div class="controls">
+                            <input type="text" id="name" class="form-control" name="name"
+                                   placeholder="请输入举报问题名称"
+                                   required
+                                   data-validation-required-message="请输入举报问题名称"
+                                   value="{{ $data->name }}">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="input-username"><span class="danger">*</span>排序 (排序值越大，在App端展示越靠前)</label>
+                        <div class="controls">
+                            <input type="text" id="input-username" class="form-control" name="sort"
+                                   placeholder="请输入排序"
+                                   required
+                                   data-validation-required-message="请输入排序"
+                                   value="{{ $data->sort }}">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="input-username"><span class="danger">*</span>使用状态</label>
+                        <div class="controls">
+                            <ul class="list-unstyled mb-0">
+                                <li class="d-inline-block mr-2 mb-1">
+                                    <fieldset>
+                                        <div class="radio radio-shadow">
+                                            <input type="radio" id="radioshadow1"  name="status" value="1" @if($data->status == 1) checked @endif>
+                                            <label for="radioshadow1">已下架</label>
+                                        </div>
+                                    </fieldset>
+                                </li>
+                                <li class="d-inline-block mr-2 mb-1">
+                                    <fieldset>
+                                        <div class="radio radio-shadow">
+                                            <input type="radio" id="radioshadow0" name="status" value="0" @if($data->status == 0) checked @endif>
+                                            <label for="radioshadow0">使用中</label>
+                                        </div>
+                                    </fieldset>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 justify-content-end">
+                    <button type="submit" class="btn btn-primary mr-1 mb-1">提交</button>
+                    <button type="reset" class="btn btn-light-secondary mr-1 mb-1">还原</button>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
+
+{{-- vendor scripts --}}
+@section('vendor-scripts')
+    <script src="{{ asset('vendors/js/extensions/moment.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/extensions/locale/zh-cn.js') }}"></script>
+    <script src="{{ asset('vendors/js/pickers/daterange/daterangepicker.js') }}"></script>
+    <script src="{{ asset('vendors/js/forms/validation/jqBootstrapValidation.js') }}"></script>
+    <script src="{{ asset('js/scripts/forms/validation/form-validation.js') }}"></script>
+@endsection
+
+{{-- page scripts --}}
+@section('page-scripts')
+    <script>
+		$(document).ready(function () {
+			$('#form').submit(function (e) {
+				e.preventDefault();
+
+				$.request({
+					url     : $(this).attr('action'),
+					type    : $(this).attr('method'),
+					data    : $(this).serialize(),
+					// debug: true,
+					callback: function (res) {
+                        if (res.code == 200) {
+                            // iframe.blade.php
+                            parent.$.hideModal();
+
+                            // iframeLayoutMaster.blade.php
+                            parent.parent.$.reloadIFrame({
+                                title  : '提交成功',
+                                message: '数据已刷新'
+                            });
+                        } else {
+                            parent.$.toast({
+                                type: 'error',
+                                title: '提交失败',
+                                message: res.msg
+                            });
+                        }
+					}
+				});
+			});
+
+		});
+    </script>
+@endsection
---
+++ resources/views/backend/bookreporttype/index.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.iframePage')
+
+{{-- page Title --}}
+@section('title','举报类型设置')
+
+{{-- vendor style --}}
+@section('vendor-styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendors/css/x-editable/bootstrap-editable.css') }}">
+@endsection
+
+@section('content')
+    <section id="config-list">
+        <div class="mb-1">
+            <a href="{{ route('backend.bookreporttype.create') }}" class="btn btn-primary glow" data-modal title="添加举报类型" role="button" aria-pressed="true">添加举报类型</a>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">@yield('title')</h4>
+            </div>
+            <div class="card-content">
+                <div class="card-body">
+                    <!-- Table with outer spacing -->
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                            <tr>
+                                <th>排序</th>
+                                <th>编号</th>
+                                <th>举报问题</th>
+                                <th>使用状态</th>
+                                <th>操作人</th>
+                                <th>更新时间</th>
+                                <th>操作</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($list as $item)
+                                <tr>
+                                    <td><span class="jeditable" data-pk="{{ $item->id }}" data-value="" > {{ $item->sort }}</td>
+                                    <td>{{$item->id }}</td>
+                                    <td>{{$item->name }}</td>
+                                    <td>{!! $item->status_type !!}</td>
+                                    <td>{{$item->admin->username}}</td>
+                                    <td>{{$item->updated_at}}</td>
+                                    <td @if($loop->count == 1)style="position: fixed;"@endif>
+                                        <div class="@if(($loop->count - $loop->iteration) < 3){{'dropup'}}@else{{'dropdown'}}@endif">
+                                            <span class="bx bx-dots-vertical-rounded font-medium-3 dropdown-toggle nav-hide-arrow cursor-pointer"
+                                                  id="dropdownMenuButton{{ $item->id }}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></span>
+                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton{{ $item->id }}">
+                                                <a class="dropdown-item" data-modal href="{{ route('backend.bookreporttype.edit', $item->id) }}" title="修改举报类型"><i class="bx bx-edit-alt mr-1"></i> 修改</a>
+                                                <a class="dropdown-item" data-destroy type="delete" href="{{ route('backend.bookreporttype.destroy', $item->id) }}" title="刪除举报类型"><i class="bx bx-trash mr-1"></i>刪除</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">总共 <b>{{ $list->appends(request()->input())->total() }}</b> 条, 分为 <b>{{ $list->lastPage() }}</b> 页</div>
+                        <div class="col-md-6">{!! $list->appends(request()->input())->links() !!}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+
+
+{{-- vendor scripts --}}
+@section('vendor-scripts')
+    <script src="{{ asset('vendors/js/x-editable/bootstrap-editable.js') }}"></script>
+@endsection
+
+{{-- page scripts --}}
+@section('page-scripts')
+    <script>
+        $(document).ready(function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                }
+            });
+
+            $.fn.editable.defaults.ajaxOptions = {type: 'PUT'};
+            $.fn.editableform.buttons = '<button type="submit" class="btn btn-primary editable-submit">确认</button>';
+
+            $('.jeditable').editable({
+                inputclass: 'form-control',
+                emptyclass: 'text-light',
+                emptytext: 'N/A',
+                placeholder: '数字需大于0',
+                url: '{{ route('backend.bookreporttype.sort') }}',
+                success: function (res, newValue) {
+                    console.log(res);
+                    parent.$.toast({
+                        title: '提交成功',
+                        message: res.msg
+                    });
+                }
+            });
+
+        });
+
+    </script>
+@endsection
+
