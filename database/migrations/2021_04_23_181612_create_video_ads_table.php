14a399dddc3e47698f2f6ebbe7956b7452888db1
max91246 <max91246@gmail.com>
2021-04-27 18:38:35 +0800

    feat: migration ad

---
+++ database/migrations/2021_04_23_181612_create_video_ads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_ads', function (Blueprint $table) {
+            $table->id();
+            $table->integer('space_id')->comment('廣告位id');
+            $table->string('name' , 100)->comment('广告名称 ');
+            $table->integer('sort')->default(0)->comment('排序');
+            $table->tinyInteger('platform')->comment('所屬平台 [1:安卓,2:IOS]');
+            $table->tinyInteger('jump_type')->comment('跳转类型 [1:内置浏览器,2:App下载,3:外部浏览器,4:站内充值页,5:不跳转]');
+            $table->string('url' , 200)->nullable()->comment('鏈結地址');
+            $table->integer('times')->default(0)->comment('顯示時間，0為不設置');
+            $table->string('image' , 200)->comment('廣告圖');
+            $table->tinyInteger('status')->comment('狀態 [-1:下架,1:上架]');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_ads');
+    }
+}
