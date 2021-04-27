1c2a831a59233cfc53112057f34a3aad1014b483
max91246 <max91246@gmail.com>
2021-04-27 18:39:28 +0800

    feat: seed UpgradeSeeder

---
+++ database/seeders/BaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BaseSeeder extends Seeder
+{
+    /**
+     * 添加视频广告位
+     *
+     * @param $name
+     */
+    protected function addAdType($name)
+    {
+        DB::table('video_ad_spaces')->truncate();
+        $exists = DB::table('video_ad_spaces')->where('name', $name)->exists();
+
+        if (!$exists) {
+            $data = [
+                'name' => $name,
+                'remark' => '',
+                'status' => 1,
+                'if_sdk_ads' => -1,
+                'created_at' => date('Y-m-d H:i:s'),
+            ];
+
+            DB::table('video_ad_spaces')->insert($data);
+        }
+    }
+}
---
+++ database/seeders/UpgradeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UpgradeSeeder extends BaseSeeder
+{
+    /**
+     * Run the database seeds.
+     * php artisan db:seed --class=UpgradeSeeder
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 升级添加广告位
+        $this->addAdType('详情页广告');
+
+    }
+}
