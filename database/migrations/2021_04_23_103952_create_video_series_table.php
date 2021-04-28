7966c99b1a72780fc2ae1071401f53f3f33409a6
RB <xtheme@gmail.com>
2021-04-28 18:26:16 +0800

    refactor: video schema

--- database/migrations/2021_04_23_103952_create_video_series_table.php
+++ database/migrations/2021_04_23_103952_create_video_series_table.php
@@ -16,12 +16,12 @@ class CreateVideoSeriesTable extends Migration
         Schema::create('video_series', function (Blueprint $table) {
             $table->id();
             $table->integer('video_id')->unsigned();
-            $table->integer('episode')->unsigned()->comment('第几集');
-            $table->string('title')->comment('名称');
-            $table->tinyInteger('free')->unsigned()->default(0)->comment('0=收费, 1=免费');
-            $table->boolean('status')->default(0)->comment('状态');
+            $table->integer('episode')->unsigned()->comment('集数');
+            $table->string('title')->comment('影集标题');
+            $table->boolean('vip')->default(0)->comment('资格: 1=VIP, -1=免费');
+            $table->boolean('status')->default(0)->comment('状态: 1=上架, -1=下架');
             $table->string('video_domain_id')->comment('视频域名');
-            $table->string('link')->comment('视频域名');
+            $table->string('link')->comment('视频链结');
             $table->string('length')->comment('视频长度');
             $table->timestamps();
         });
