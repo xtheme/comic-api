f16c0766bb15d36e512e3746c29c9c7d018c8e32
max <max91246@gmail.com>
2021-05-04 14:56:11 +0800

    feat: add block migration

---
+++ database/migrations/2021_04_29_145048_create_blocks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title' , 100)->comment('标题');
+            $table->integer('sort' )->default(0)->comment('排序');
+            $table->integer('focus')->comment('聚焦数量');
+            $table->integer('row')->comment('行数量');
+            $table->string('causer', 191)->nullable();
+            $table->json('properties')->nullable();
+            $table->tinyInteger('status')->comment('狀態 [-1:下架,1:上架]');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+}
