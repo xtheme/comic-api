fbb632eed1998554c037d39256ad12c6cb3ae75f
RB <xtheme@gmail.com>
2021-05-10 19:21:54 +0800

    chore: histories backup

---
+++ database/migrations/2021_05_07_172749_create_views_member_histories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Staudenmeir\LaravelMigrationViews\Facades\Schema;
+
+class CreateViewsMemberHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $query = DB::table('histories')->select([
+            'minor_id',
+            'class',
+            'type',
+        ])->selectRaw('COUNT(*) as count')
+            ->where('user_vip', 1)
+            ->where('major_id', '!=', 0)
+            ->groupBy([
+                'minor_id',
+                'class',
+                'type',
+            ]);
+
+        Schema::createOrReplaceView('views_member_histories', $query);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropViewIfExists('views_member_histories');
+    }
+}
---
+++ database/migrations/2021_05_07_172913_create_views_guest_histories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Staudenmeir\LaravelMigrationViews\Facades\Schema;
+
+class CreateViewsGuestHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     *
+     */
+    public function up()
+    {
+        $query = DB::table('histories')->select([
+            'minor_id',
+            'class',
+            'type',
+        ])
+            ->selectRaw('COUNT(*) as count')
+            ->where('user_vip', -1)
+            ->where('major_id', '!=', 0)
+            ->groupBy([
+                'minor_id',
+                'class',
+                'type',
+            ]);
+
+        Schema::createOrReplaceView('views_guest_histories', $query);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropViewIfExists('views_guest_histories');
+    }
+}
