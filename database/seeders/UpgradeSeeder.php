cba3ca05e2f59a159e81ae5bab5713c461f6534f
RB <xtheme@gmail.com>
2021-05-11 16:53:24 +0800

    refactor: UpgradeSeeder

--- database/seeders/UpgradeSeeder.php
+++ database/seeders/UpgradeSeeder.php
@@ -18,20 +18,9 @@ class UpgradeSeeder extends Seeder
         // 升级添加广告位
         $this->addAdSpace('动画详情页广告' , 'video');
 
-        // 動畫 CDN 域名
-        DB::table('video_domains')->truncate();
+        // 升级 CDN 域名
+        $this->addCdn();
 
-        $data = [
-            'title' => '茄子線上和UAT共用域名',
-            'domain' => 'https://qzvd-blibli4.6b0e3d.com',
-            'encrypt_domain' => 'https://qzvd-hw.testzone.cn',
-            'remark' => '',
-            'sort' => 0,
-            'status' => 1,
-            'created_at' => date('Y-m-d H:i:s'),
-        ];
-
-        DB::table('video_domains')->insert($data);
     }
 
     /**
@@ -55,6 +44,25 @@ class UpgradeSeeder extends Seeder
             ];
 
             DB::table('ad_spaces')->insert($data);
+        }
+    }
+
+    protected function addCdn()
+    {
+        $exists = DB::table('video_domains')->where('title', '茄子線上和UAT共用域名')->exists();
+
+        if (!$exists) {
+            $data = [
+                'title' => '茄子線上和UAT共用域名',
+                'domain' => 'https://qzvd-blibli4.6b0e3d.com',
+                'encrypt_domain' => 'https://qzvd-hw.testzone.cn',
+                'remark' => '',
+                'sort' => 0,
+                'status' => 1,
+                'created_at' => date('Y-m-d H:i:s'),
+            ];
+
+            DB::table('video_domains')->insert($data);
         }
     }
 }
