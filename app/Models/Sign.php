afb3645f2f6f2b2bff3c144e3edb2b47dc28f19e
max <max91246@gmail.com>
2021-05-20 10:05:47 +0800

    feat: sign 流程重新处理

--- app/Models/Sign.php
+++ app/Models/Sign.php
@@ -2,35 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Models\Sign
- *
- * @property int $id
- * @property int $uid 用户id
- * @property int $days 签到天数
- * @property string $addtime 签到时间
- * @property-read \App\Models\User|null $user
- * @method static \Illuminate\Database\Eloquent\Builder|Sign newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Sign newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Sign query()
- * @method static \Illuminate\Database\Eloquent\Builder|Sign whereAddtime($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Sign whereDays($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Sign whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Sign whereUid($value)
- * @mixin \Eloquent
- */
 class Sign extends BaseModel
 {
-    protected $table = 'sign';
-
-    public $timestamps = false;
-
     protected $fillable = [
-        'days',
-        'addtime'
+        'user_id',
+        'created_at'
     ];
 
     public function user()
