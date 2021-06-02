c8c57b82aee6f641aaa453c51f06df20fbaf4ee9
RB <xtheme@gmail.com>
2021-06-02 13:45:15 +0800

    refactor: 遷移漫畫數據腳本優化

--- app/Console/Commands/Migrate/BookChaptersMigrate.php
+++ app/Console/Commands/Migrate/BookChaptersMigrate.php
@@ -6,6 +6,7 @@ ini_set('memory_limit', '-1');
 
 use App\Models\BookChapter;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class BookChaptersMigrate extends Command
@@ -41,37 +42,64 @@ class BookChaptersMigrate extends Command
      */
     public function handle()
     {
-        if ($this->confirm('请确认是否执行数据迁移？')) {
+        // 每次轉詞數據量
+        $batch_num = 10000;
 
+        // 每多少筆切割一次操作
+        $chunk_num = 500;
+
+        // 新舊數據表名稱
+        $old_table = 'chapterlist';
+        $new_table = 'book_chapters';
+
+        // 目前用戶表最後 uid
+        $old_primary_id = DB::table($old_table)->orderByDesc('id')->first()->id;
+
+        // 新表最後 uid
+        $new_primary_id = DB::table($new_table)->orderByDesc('id')->first()->id;
+
+        if ($old_primary_id > $new_primary_id) {
             // 分割集合
-            $data = DB::table('chapterlist')->get()->chunk(50);
+            $data = DB::table($old_table)->where('id', '>', $new_primary_id)->limit($batch_num)->get()->chunk($chunk_num);
 
-            $this->line('共有' . count($data) . '批数据等待迁移!');
+            $this->line(sprintf('為了避免腳本超時，本次操作將轉移 %s 筆數據，共拆分為 %s 批数据進行迁移！', $batch_num, ceil($batch_num / $chunk_num)));
 
-            $data->each(function($chapters, $key) {
-                $insert = $chapters->map(function($chapter) {
+            $data->each(function($items, $key) use ($new_table) {
+                $insert = $items->map(function($item) use ($new_table) {
                     return [
-                        'id' => $chapter->id,
-                        'book_id' => $chapter->book_id,
-                        'episode' => $chapter->idx,
-                        'title' => $chapter->title,
-                        'content' => $chapter->content,
-                        'json_images' => $chapter->json_images ?? '',
-                        'status' => $chapter->status ? 1 : -1,
-                        'charge' => $chapter->isvip ? 1 : -1,
-                        'review' => $chapter->check_status + 1,
-                        'operating' => $chapter->operating,
-                        'created_at' => date('Y-m-d H:i:S', $chapter->addtime),
-                        'updated_at' => date('Y-m-d H:i:S', $chapter->updatetime),
+                        'id' => $item->id,
+                        'book_id' => $item->book_id,
+                        'episode' => $item->idx,
+                        'title' => $item->title,
+                        'content' => $item->content,
+                        'json_images' => $item->json_images ?? parseImgFromHtml($item->content),
+                        'status' => $item->status ? 1 : -1,
+                        'charge' => $item->isvip ? 1 : -1,
+                        'review' => $item->check_status + 1,
+                        'operating' => $item->operating,
+                        'created_at' => $item->addtime ? Carbon::createFromTimestamp($item->addtime) : null,
+                        'updated_at' => $item->updatetime ? Carbon::createFromTimestamp($item->updatetime) : null,
                     ];
                 })->toArray();
 
-                BookChapter::insert($insert);
+                DB::table($new_table)->insert($insert);
 
-                $this->line('第' . ($key + 1) . '批数据迁移完成');
+                $this->line('第 ' . ($key + 1) . ' 批数据迁移完成...');
             });
 
-            $this->line('数据迁移完成');
+            $this->line('本次數據已全數遷移！');
+
+            $latest_primary_id = DB::table($new_table)->orderByDesc('id')->first()->id;
+
+            $pending_num = DB::table($old_table)->where('id', '>', $latest_primary_id)->count();
+
+            if ($this->confirm('尚有' . $pending_num . '筆數據等待遷移，是否繼續執行此腳本？')) {
+                $this->call('migrate:book_chapters');
+            } else {
+                $this->line('操作已結束');
+            }
+        } else {
+            $this->line('目前沒有等待遷移的數據，操作已結束');
         }
 
         return 0;
--- app/Console/Commands/Migrate/BooksMigrate.php
+++ app/Console/Commands/Migrate/BooksMigrate.php
@@ -6,6 +6,7 @@ ini_set('memory_limit', '-1');
 
 use App\Models\Book;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class BooksMigrate extends Command
@@ -41,40 +42,66 @@ class BooksMigrate extends Command
      */
     public function handle()
     {
-        if ($this->confirm('请确认是否运行重构脚本? 执行且请先备份数据表!')) {
+        // 每次轉詞數據量
+        $batch_num = 10000;
 
+        // 每多少筆切割一次操作
+        $chunk_num = 500;
+
+        // 新舊數據表名稱
+        $old_table = 'book';
+        $new_table = 'books';
+
+        // 目前用戶表最後 uid
+        $old_primary_id = DB::table($old_table)->orderByDesc('id')->first()->id;
+
+        // 新表最後 uid
+        $new_primary_id = DB::table($new_table)->orderByDesc('id')->first()->id;
+
+        if ($old_primary_id > $new_primary_id) {
             // 分割集合
-            $data = DB::table('book')->get()->chunk(1000);
+            $data = DB::table($old_table)->where('id', '>', $new_primary_id)->limit($batch_num)->get()->chunk($chunk_num);
 
-            $this->line('共有' . count($data) . '批数据等待迁移!');
+            $this->line(sprintf('為了避免腳本超時，本次操作將轉移 %s 筆數據，共拆分為 %s 批数据進行迁移！', $batch_num, ceil($batch_num / $chunk_num)));
 
-            $data->each(function($books, $key) {
-                $insert = $books->map(function($book) {
+            $data->each(function($items, $key) use ($new_table) {
+                $insert = $items->map(function($item) use ($new_table) {
                     return [
-                        'id' => $book->id,
-                        'title' => $book->book_name,
-                        'author' => $book->pen_name,
-                        'description' => $book->book_desc,
-                        'end' => $book->book_isend == 1 ? 1 : -1,
-                        'vertical_cover' => $book->book_thumb,
-                        'horizontal_cover' => $book->book_thumb2,
-                        'type' => $book->cartoon_type,
-                        'visits' => $book->view,
-                        'status' => $book->book_status ? 1 : -1,
-                        // 'charge' => $book->book_vip ? 1 : -1,
-                        'review' => $book->check_status + 1,
-                        'operating' => $book->operating,
-                        'created_at' => date('Y-m-d H:i:S', $book->book_addtime),
-                        'updated_at' => date('Y-m-d H:i:S', $book->book_updatetime),
+                        'id' => $item->id,
+                        'title' => $item->book_name,
+                        'author' => $item->pen_name,
+                        'description' => $item->book_desc,
+                        'end' => $item->book_isend == 1 ? 1 : -1,
+                        'vertical_cover' => $item->book_thumb,
+                        'horizontal_cover' => $item->book_thumb2,
+                        'type' => $item->cartoon_type,
+                        'visits' => $item->view,
+                        'status' => $item->book_status ? 1 : -1,
+                        'review' => $item->check_status + 1,
+                        'operating' => $item->operating,
+                        'created_at' => $item->book_addtime ? Carbon::createFromTimestamp($item->book_addtime) : null,
+                        'updated_at' => $item->book_updatetime ? Carbon::createFromTimestamp($item->book_updatetime) : null,
                     ];
                 })->toArray();
 
-                Book::insert($insert);
+                DB::table($new_table)->insert($insert);
 
-                $this->line('第' . ($key + 1) . '批数据迁移完成');
+                $this->line('第 ' . ($key + 1) . ' 批数据迁移完成...');
             });
 
-            $this->line('数据迁移完成');
+            $this->line('本次數據已全數遷移！');
+
+            $latest_primary_id = DB::table($new_table)->orderByDesc('id')->first()->id;
+
+            $pending_num = DB::table($old_table)->where('id', '>', $latest_primary_id)->count();
+
+            if ($this->confirm('尚有' . $pending_num . '筆數據等待遷移，是否繼續執行此腳本？')) {
+                $this->call('migrate:books');
+            } else {
+                $this->line('操作已結束');
+            }
+        } else {
+            $this->line('目前沒有等待遷移的數據，操作已結束');
         }
 
         return 0;
