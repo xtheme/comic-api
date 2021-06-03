8352d2bd0dfd01d7f2fd3cd7bfaac9cecf15cb7f
RB <xtheme@gmail.com>
2021-06-03 16:45:33 +0800

    feat: Admin 模型兼容調整

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,12 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class Admin extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use HasRoles;
 
     protected $table = 'admin';
+
+    // todo User 欄位調整
+    const CREATED_AT = 'create_time';
+    const UPDATED_AT = 'update_time';
+
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
@@ -19,8 +27,13 @@ class Admin extends Authenticatable
      */
     protected $fillable = [
         'username',
-        'email',
         'password',
+        'logintime',
+        'loginip',
+        'status',
+        'nickname',
+        'create_time',
+        'update_time',
     ];
 
     /**
@@ -30,6 +43,20 @@ class Admin extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+        'salt',
+        'image',
+        'token',
+    ];
+
+    protected $dates = [
+        'logintime',
+        'create_time',
+        'update_time'
+    ];
+
+    protected $casts = [
+        'logintime' => 'datetime',
+        'create_time' => 'datetime',
+        'update_time' => 'datetime'
     ];
 }
