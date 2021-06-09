e498ea5e77ee190d830ffa1a1ae71ada5669fbaa
RB <xtheme@gmail.com>
2021-06-10 02:08:10 +0800

    refactor: 拿掉非必要的查詢條件

--- app/Repositories/VideoRepository.php
+++ app/Repositories/VideoRepository.php
@@ -49,7 +49,8 @@ class VideoRepository extends Repository implements VideoRepositoryInterface
         $page = $request->input('page') ?? 1;
         $perPage = $request->input('perPage') ?? 10;
 
-        return $this->model::with(['series', 'series.cdn'])->withCount(['series' , 'visit_histories' , 'play_histories'])->when($title, function (Builder $query, $title) {
+        // return $this->model::with(['series', 'series.cdn'])->withCount(['series' , 'visit_histories' , 'play_histories'])->when($title, function (Builder $query, $title) {
+        return $this->model::withCount(['series' , 'visit_histories' , 'play_histories'])->when($title, function (Builder $query, $title) {
             return $query->whereLike('title', $title);
         })->when($author, function (Builder $query, $author) {
             return $query->whereLike('author', $author);
