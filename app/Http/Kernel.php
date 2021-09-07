781ed3fd6333a7e7dac6c95e1f82474f9243b2b7
RB <xtheme@gmail.com>
2021-09-07 11:59:04 +0800

    chore: 重整格式 Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,78 +2,74 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\AuthRouteRole;
-use App\Http\Middleware\LogActivity;
-use App\Http\Middleware\SingleSignOn;
-use App\Http\Middleware\VerifyApiHeader;
-use App\Http\Middleware\VerifyApiSign;
-use App\Http\Middleware\VerifyJwtToken;
+use App\Http\Middleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-  /**
-   * The application's global HTTP middleware stack.
-   *
-   * These middleware are run during every request to your application.
-   *
-   * @var array
-   */
-  protected $middleware = [
-    // \App\Http\Middleware\TrustHosts::class,
-    \App\Http\Middleware\TrustProxies::class,
-    \Fruitcake\Cors\HandleCors::class,
-    \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-    \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-    \App\Http\Middleware\TrimStrings::class,
-    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-  ];
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
 
-  /**
-   * The application's route middleware groups.
-   *
-   * @var array
-   */
-  protected $middlewareGroups = [
-    'web' => [
-      \App\Http\Middleware\EncryptCookies::class,
-      \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-      \Illuminate\Session\Middleware\StartSession::class,
-      // \Illuminate\Session\Middleware\AuthenticateSession::class,
-      \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-      \App\Http\Middleware\VerifyCsrfToken::class,
-      \Illuminate\Routing\Middleware\SubstituteBindings::class,
-      \App\Http\Middleware\LocaleMiddleware::class,
-    ],
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\LocaleMiddleware::class,
+        ],
 
-    'api' => [
-      'throttle:api',
-      \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    ],
-  ];
+        'api' => [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
 
-  /**
-   * The application's route middleware.
-   *
-   * These middleware may be assigned to groups or used individually.
-   *
-   * @var array
-   */
-  protected $routeMiddleware = [
-    'auth' => \App\Http\Middleware\Authenticate::class,
-    'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-    'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-    'can' => \Illuminate\Auth\Middleware\Authorize::class,
-    'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-    'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-    'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-    // 'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-    'throttle' => \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class,
-    'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    'api.sign' => VerifyApiSign::class,
-    'api.token' => VerifyJwtToken::class,
-    'device.sso' => SingleSignOn::class,
-    'auth.route.role' => AuthRouteRole::class,
-    'log.activity' => LogActivity::class,
-  ];
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth'             => \App\Http\Middleware\Authenticate::class,
+        'auth.basic'       => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'cache.headers'    => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can'              => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'            => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed'           => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        // 'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'throttle'         => \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class,
+        'verified'         => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'api.header'       => Middleware\VerifyApiHeader::class,
+        'api.sign'         => Middleware\VerifyApiSign::class,
+        'api.token'        => Middleware\VerifyJwtToken::class,
+        'device.sso'       => Middleware\SingleSignOn::class,
+        'auth.route.role'  => Middleware\AuthRouteRole::class,
+        'log.activity'     => Middleware\LogActivity::class,
+    ];
 }
