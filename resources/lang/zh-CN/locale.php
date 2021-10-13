1e9635941176197185cdbc36ed9b88857c767a60
RB <xtheme@gmail.com>
2021-10-13 23:08:59 +0800

    feat: 菜單調整

--- resources/lang/zh-CN/locale.php
+++ resources/lang/zh-CN/locale.php
@@ -1,15 +1,9 @@
 <?php
 
 return [
-    "Site Title" => "茄子漫画社",
+    "Site Title" => "色多多",
 
     "Dashboard" => "控制台",
-
-    "Systems" => "系统设定",
-    "Configs" => "配置项",
-    "Navigation" => "导航推荐",
-    "Activity Log" => "操作日志",
-    "System Log" => "系统日志",
 
     "Customers" => "会员管理",
     "Customer List" => "会员列表",
@@ -23,31 +17,29 @@ return [
 
     "Books" => "漫画管理",
     "Books List" => "漫画列表",
-    "Recommended Books" => "推荐漫画",
-    "CDN Caching List" => "CDN 预热名单",
+    // "Recommended Books" => "推荐漫画",
+    // "CDN Caching List" => "CDN 预热名单",
 
     "Report" => "举报管理",
     "Report Type" => "举报类型",
     "Report List" => "举报列表",
 
-    "Feedback" => "意見反饋",
+    // "Feedback" => "意見反饋",
 
-    "Categories" => "分类管理",
-    "Tags" => "标签分类",
-    "Topic List" => "主题模块",
+    "Homepage" => "首页配置",
+    "Navigation" => "导航推荐",
+    "Tags" => "标签设定",
+    "Topics" => "主题区块",
 
     "Videos" => "动画管理",
     "Video List" => "动画列表",
     "Video Domain" => "动画域名配置",
     "Play Statistics" => "播放统计",
+    "Comment List" => "评论列表",
 
-    "Ad" => "广告与公告",
+    "Ad" => "站内广告",
     "AdPlace List" => "广告位列表",
     "Ad List" => "广告列表",
-    "Notice List" => "公告列表",
-
-    "Comments" => "評論管理",
-    "Comment List" => "漫画评论",
 
     "Book Report" => "漫画举报管理",
     "ReportType List" => "举报类型设置",
@@ -55,7 +47,12 @@ return [
     "Permission Manage" => "权限管理",
     "Administrator list" => "管理员列表",
     "Role List" => "角色列表",
-    "Permission List" => "权限列表",
+
+    "Systems" => "系统设定",
+    "Configs" => "配置项",
+    "Notice List" => "公告列表",
+    "Activity Log" => "操作日志",
+    "System Log" => "系统日志",
 
     "logout" => "退出登录",
 ];
