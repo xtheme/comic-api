5f6bcd0ba7c896f24acebc4214fdf55ba0b98a79
RB <xtheme@gmail.com>
2021-10-24 22:43:49 +0800

    fix(api): 搜尋與主題使用橫向封面

--- app/Http/Controllers/Api/TopicController.php
+++ app/Http/Controllers/Api/TopicController.php
@@ -24,7 +24,7 @@ class TopicController extends BaseController
                 'id' => $item->id,
                 'title' => $item->title,
                 'author' => $item->author,
-                'cover' => ($type == 'book') ? $item->vertical_cover : $item->cover,
+                'cover' => ($type == 'book') ? $item->horizontal_cover : $item->cover,
                 'tagged_tags' => $item->tagged_tags,
                 'view_counts' => shortenNumber($item->view_counts),
                 'created_at' => optional($item->created_at)->format('Y-m-d'),
@@ -52,7 +52,7 @@ class TopicController extends BaseController
                 'filter_id' => $topic->filter_id,
                 'spotlight' => $topic->spotlight,
                 'per_line' => $topic->row,
-                'list' => $this->arrangeData($topic->type, $list, $topic->row),
+                'list' => $this->arrangeData($topic->type, $list),
             ];
         });
 
