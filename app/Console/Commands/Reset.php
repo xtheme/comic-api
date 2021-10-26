954faa00aa28e8ab36fd6c7a2843d75db42141df
RB <xtheme@gmail.com>
2021-10-26 21:46:58 +0800

    feat: reset data command

---
+++ app/Console/Commands/Reset.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Channel;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class Reset extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '數據初始化';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if ($this->confirm('是否確認要將數據初始化?')) {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+            DB::table('personal_access_tokens')->truncate();
+            DB::table('ranking_logs')->truncate();
+            DB::table('users')->truncate();
+            DB::table('user_favorite_logs')->truncate();
+            DB::table('user_purchase_logs')->truncate();
+            DB::table('user_recharge_logs')->truncate();
+            DB::table('user_visit_logs')->truncate();
+            DB::table('orders')->truncate();
+            DB::table('channels')->truncate();
+            DB::table('channel_daily_reports')->truncate();
+            DB::table('channel_monthly_reports')->truncate();
+            DB::table('payment_daily_reports')->truncate();
+            DB::table('payment_monthly_reports')->truncate();
+            DB::table('admins')->truncate();
+            DB::table('model_has_permissions')->truncate();
+            DB::table('model_has_roles')->truncate();
+            DB::table('permissions')->truncate();
+            DB::table('roles')->truncate();
+            DB::table('role_has_permissions')->truncate();
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+            DB::unprepared(file_get_contents(database_path('sql/install.sql')));
+        }
+
+        $this->info('目前处于开发模式下, 正式发布前请执行 php artisan release 优化代码');
+    }
+}
