a254b40cd8fb7ca2b1fed4ad0783078022218f2f
RB <xtheme@gmail.com>
2021-11-04 01:47:38 +0800

    feat: safe reset command

--- app/Console/Commands/Reset.php
+++ app/Console/Commands/Reset.php
@@ -39,35 +39,36 @@ class Reset extends Command
     {
         if ($this->confirm('是否確認要將數據初始化?')) {
             DB::statement('SET FOREIGN_KEY_CHECKS=0');
-            DB::table('migrations')->truncate();
             DB::table('personal_access_tokens')->truncate();
-            DB::table('ranking_logs')->truncate();
             DB::table('users')->truncate();
             DB::table('user_favorite_logs')->truncate();
             DB::table('user_purchase_logs')->truncate();
             DB::table('user_recharge_logs')->truncate();
             DB::table('user_visit_logs')->truncate();
             DB::table('orders')->truncate();
-            DB::table('channels')->truncate();
             DB::table('channel_daily_reports')->truncate();
             DB::table('channel_monthly_reports')->truncate();
             DB::table('payment_daily_reports')->truncate();
             DB::table('payment_monthly_reports')->truncate();
-            DB::table('admins')->truncate();
-            DB::table('model_has_permissions')->truncate();
-            DB::table('model_has_roles')->truncate();
-            DB::table('permissions')->truncate();
-            DB::table('roles')->truncate();
-            DB::table('role_has_permissions')->truncate();
-            DB::table('topics')->truncate();
-            DB::table('navigation')->truncate();
-            DB::table('payments')->truncate();
-            DB::table('pricings')->truncate();
-            DB::table('categories')->truncate();
-            DB::table('tags')->truncate();
-            DB::table('configs')->truncate();
-            DB::table('activity_log')->truncate();
-            DB::unprepared(file_get_contents(database_path('sql/install.sql')));
+            DB::table('ranking_logs')->truncate();
+
+            // DB::table('channels')->truncate();
+            // DB::table('admins')->truncate();
+            // DB::table('model_has_permissions')->truncate();
+            // DB::table('model_has_roles')->truncate();
+            // DB::table('permissions')->truncate();
+            // DB::table('roles')->truncate();
+            // DB::table('role_has_permissions')->truncate();
+            // DB::table('topics')->truncate();
+            // DB::table('navigation')->truncate();
+            // DB::table('payments')->truncate();
+            // DB::table('pricings')->truncate();
+            // DB::table('categories')->truncate();
+            // DB::table('tags')->truncate();
+            // DB::table('configs')->truncate();
+            // DB::table('activity_log')->truncate();
+            // DB::table('migrations')->truncate();
+            // DB::unprepared(file_get_contents(database_path('sql/install.sql')));
             DB::statement('SET FOREIGN_KEY_CHECKS=1');
         }
 
