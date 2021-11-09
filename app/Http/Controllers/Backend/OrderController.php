f94bc0e07be0859bbfee3b48bef27d8d23f71780
RB <xtheme@gmail.com>
2021-11-09 11:59:11 +0800

    feat: OrderRepository bye

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -6,25 +6,55 @@ use App\Enums\OrderOptions;
 use App\Http\Controllers\Controller;
 use App\Jobs\RechargeJob;
 use App\Models\Order;
-use App\Repositories\Contracts\OrderRepositoryInterface;
 use App\Services\UserService;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 
 class OrderController extends Controller
 {
-    private $repository;
-
-    public function __construct(OrderRepositoryInterface $repository)
+    private function filter(Request $request): Builder
     {
-        $this->repository = $repository;
+        $order_no = $request->get('order_no') ?? '';
+        $transaction_id = $request->get('transaction_id') ?? '';
+        $user_id = $request->get('user_id') ?? '';
+        $type = $request->get('type') ?? '';
+        $status = $request->get('status') ?? null;
+        $platform = $request->get('platform') ?? 0;
+        $version = $request->get('version') ?? 0;
+        $created_at = $request->get('created_at') ?? '';
+
+        return Order::has('user')->with(['user', 'payment'])->when($order_no, function (Builder $query, $order_no) {
+            return $query->where('order_no', $order_no);
+        })->when($transaction_id, function (Builder $query, $transaction_id) {
+            return $query->where('transaction_id', $transaction_id);
+        })->when($user_id, function (Builder $query, $user_id) {
+            return $query->where('user_id', $user_id);
+        })->when($type, function (Builder $query, $type) {
+            return $query->where('type', $type);
+        })->when($status, function (Builder $query, $status) {
+            return $query->where('status', $status);
+        })->when($platform, function (Builder $query, $platform) {
+            return $query->where('platform', $platform);
+        })->when($version, function (Builder $query, $version) {
+            return $query->where('version', $version);
+        })->when($created_at, function (Builder $query, $created_at) {
+            $date = explode(' - ', $created_at);
+            $start_date = $date[0];
+            $end_date = $date[1];
+
+            return $query->whereBetween('created_at', [
+                $start_date,
+                $end_date,
+            ]);
+        })->latest();
     }
 
     public function index(Request $request)
     {
         $data = [
-            'list' => $this->repository->filter($request)->paginate(),
+            'list' => $this->filter($request)->paginate(),
             'type_options' => OrderOptions::TYPE_OPTIONS,
             'status_options' => OrderOptions::STATUS_OPTIONS,
             'pageConfigs' => ['hasSearchForm' => true],
@@ -36,7 +66,7 @@ class OrderController extends Controller
     public function detail($order_id)
     {
         $data = [
-            'order' => $this->repository->find($order_id),
+            'order' => Order::findOrFail($order_id),
         ];
 
         return view('backend.order.detail')->with($data);
