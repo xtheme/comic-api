dd11806b5e55146f0cfb6122f8711556e1bd5d51
RB <xtheme@gmail.com>
2021-11-09 13:22:43 +0800

    refactor: ResumeRepository bye

--- app/Http/Controllers/Api/ResumeController.php
+++ app/Http/Controllers/Api/ResumeController.php
@@ -2,26 +2,63 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Resume;
 use App\Models\ResumeCity;
 use App\Models\Movie;
-use App\Repositories\Contracts\ResumeRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
 class ResumeController extends BaseController
 {
-    private $repository;
-
-    public function __construct(ResumeRepositoryInterface $repository)
-    {
-        $this->repository = $repository;
-    }
-
     public function cities(Request $request)
     {
         $collect = ResumeCity::with(['children', 'children.children'])->where('p_id', 0)->get();
 
         return Response::jsonSuccess(__('api.success'), $collect);
+    }
+
+    private function filter(Request $request): Builder
+    {
+        $city_id = $request->input('city_id') ?? null;
+        $state = $request->input('state') ?? '';
+        $order = $request->input('order') ?? 'updated_at';
+        $sort = $request->input('sort') ?? 'desc';
+        $limit = $request->input('limit') ?? 10;
+
+        return Resume::when($city_id, function (Builder $query, $city_id) {
+            return $query->where('city_id', $city_id);
+        })->when($state, function (Builder $query, $state) {
+            return $query->where('state', $state);
+        })->when($order, function (Builder $query, $order) use ($sort) {
+            if ($sort == 'desc') {
+                return $query->orderByDesc($order);
+            } else {
+                return $query->orderBy($order);
+            }
+        })->take($limit);
+    }
+
+    // todo use api resource
+    private function format(Resume $lady): array
+    {
+        $data =  [
+            'id' => $lady->id,
+            'name' => $lady->name,
+            'city_id' => $lady->city_id,
+            'age' => $lady->age,
+            'sale_price' => $lady->sale_price,
+            'price' => $lady->price,
+            'qq' => $lady->qq,
+            'wechat' => $lady->wechat,
+            'phone' => $lady->phone,
+            'tag' => $lady->tag,
+            'profile' => $lady->profile,
+            'pictures' => $lady->pictures,
+            'updated_at' => $lady->updated_at->diffForHumans(),
+        ];;
+
+        return $data;
     }
 
     public function list(Request $request, $city = null)
@@ -33,9 +70,11 @@ class ResumeController extends BaseController
             'sort'    => 'desc',
         ]);
 
-        $collect = $this->repository->filter($request)->get();
+        $collect = $this->filter($request)->get();
 
-        $data = $this->repository->collectFormat($collect);
+        $data = $collect->map(function($video) {
+            return $this->format($video);
+        })->toArray();
 
         return Response::jsonSuccess(__('api.success'), $data);
     }
@@ -44,7 +83,7 @@ class ResumeController extends BaseController
     {
         $video = Movie::find($id);
 
-        $data = $this->repository->format($video);
+        $data = $this->format($video);
 
         // 訪問數+1
         $video->increment('views');
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -19,8 +19,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(Contracts\TagRepositoryInterface::class, Repositories\TagRepository::class);
         $this->app->bind(Contracts\VideoRepositoryInterface::class, Repositories\VideoRepository::class);
         $this->app->bind(Contracts\TopicRepositoryInterface::class, Repositories\TopicRepository::class);
-        $this->app->bind(Contracts\ResumeRepositoryInterface::class, Repositories\ResumeRepository::class);
-
     }
 
     /**
