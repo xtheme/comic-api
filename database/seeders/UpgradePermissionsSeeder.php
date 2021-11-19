d9ead85d5666ec1c824dea64900ad3e6d3be25bb
RB <xtheme@gmail.com>
2021-11-19 13:58:44 +0800

    fix: UpgradePermissionsSeeder

--- database/seeders/UpgradePermissionsSeeder.php
+++ database/seeders/UpgradePermissionsSeeder.php
@@ -20,11 +20,32 @@ class UpgradePermissionsSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('model_has_permissions')->truncate();
+        DB::table('model_has_roles')->truncate();
+        DB::table('permissions')->truncate();
+        DB::table('roles')->truncate();
+        DB::table('role_has_permissions')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $this->addRoles();
 
         $this->addPermissions();
 
         $this->setSuperAdmin();
+    }
+
+    /**
+     * 建立角色
+     */
+    protected function addRoles()
+    {
+        Role::create(['name' => '管理员']);
+        Role::create(['name' => '运营']);
+        Role::create(['name' => '客服']);
+        Role::create(['name' => '财务']);
     }
 
     /**
@@ -36,7 +57,19 @@ class UpgradePermissionsSeeder extends Seeder
         return collect(Route::getRoutes())->filter(function ($route) {
             return Str::startsWith($route->getName(), 'backend');
         })->filter(function ($route) {
-            $abilities = ['index', 'create', 'edit', 'destroy', 'review', 'preview', 'export', 'editable', 'batch', 'transfer', 'unbind'];
+            $abilities = [
+                'index',
+                'create',
+                'edit',
+                'destroy',
+                'review',
+                'preview',
+                'export',
+                'editable',
+                'batch',
+                'daily',
+                'revenue',
+            ];
             $name = last(explode('.', $route->getName()));
             return Str::endsWith($name, $abilities);
         })->map(function ($route) {
