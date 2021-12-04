31e69a29dd99d6a4efe91d15d81e2b66494de87a
RB <xtheme@gmail.com>
2021-12-04 17:10:59 +0800

    feat: 簡化驗證碼規則

--- config/captcha.php
+++ config/captcha.php
@@ -1,14 +1,17 @@
 <?php
 
 return [
-    'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
+    // 'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
+    'characters' => ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     'default' => [
-        'length' => 9,
+        'length' => 4,
         'width' => 120,
         'height' => 30,
         'quality' => 90,
-        'math' => true,
+        'math' => false,
         'expire' => 120,
+        'bgImage' => false,
+        'bgColor' => '#ecf2f4',
         'encrypt' => false,
     ],
     'math' => [
