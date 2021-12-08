a6c39e69de7ebd9db8a4c49aed2a2b73c57cca94
RB <xtheme@gmail.com>
2021-12-08 14:51:35 +0800

    fix: 開啟 API 限流

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -57,8 +57,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            // return Limit::perMinute(200)->by(optional($request->user())->id ?: $request->ip());
-            return Limit::none();
+            return Limit::perMinute(200)->by(optional($request->user())->id ?: $request->ip());
+            // return Limit::none();
         });
     }
 }
