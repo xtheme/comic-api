bbd4604f7f3465b446886d9f6afc4b791fcc40f4
RB <xtheme@gmail.com>
2021-12-10 00:40:48 +0800

    fix: json_images null

--- app/Models/BookChapter.php
+++ app/Models/BookChapter.php
@@ -33,7 +33,7 @@ class BookChapter extends BaseModel
     // 將 json_images 字段中的圖片路徑加上資源域名, 如果使用加密資源則指定圖片寬度
     public function getContentAttribute()
     {
-        $images = $this->json_images;
+        $images = $this->json_images ?? [];
 
         foreach ($images as $key => $image) {
             if (Str::startsWith($image, '/') ) {
