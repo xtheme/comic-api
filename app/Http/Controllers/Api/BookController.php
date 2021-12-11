4d12f6690149c63dd91c0d8fcd9bbcfc40e6f0a7
RB <xtheme@gmail.com>
2021-12-11 14:39:26 +0800

    retactor: 優化漫畫章節緩存順序

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Resources\BookChapterResource;
 use App\Jobs\VisitBook;
+use App\Jobs\VisitBookChapter;
 use App\Models\Book;
 use App\Models\BookChapter;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -20,18 +21,18 @@ class BookController extends BaseController
         }
     }
 
-    public function detail(Request $request, $id)
+    public function detail(Request $request, $book_id)
     {
-        $cache_key = 'book:' . $id;
+        $cache_key = 'book:' . $book_id;
 
-        $data = Cache::remember($cache_key, 1, function () use ($id) {
+        $data = Cache::remember($cache_key, 28800, function () use ($book_id) {
             try {
                 $book = Book::with([
                     'chapters' => function ($query) {
                         $query->where('status', 1)->oldest('episode');
                     },
                     'favorite_logs',
-                ])->withCount(['chapters'])->findOrFail($id);
+                ])->withCount(['chapters'])->findOrFail($book_id);
 
                 return [
                     'id' => $book->id,
@@ -46,7 +47,7 @@ class BookController extends BaseController
                     'collect_counts' => shortenNumber($book->collect_counts),
                     'has_favorite' => false,
                     'chapters_count' => $book->chapters_count,
-                    'chapters' => $book->chapters->map(function($chapter) {
+                    'chapters' => $book->chapters->map(function ($chapter) {
                         return json_decode((new BookChapterResource($chapter))->purchased(false)->toJson(), true);
                     })->toArray(),
                 ];
@@ -59,7 +60,7 @@ class BookController extends BaseController
 
         if ($user) {
             // 收藏紀錄
-            $data['has_favorite'] = $user->favorite_logs()->where('type', 'book')->where('item_id', $id)->exists();
+            $data['has_favorite'] = $user->favorite_logs()->where('type', 'book')->where('item_id', $book_id)->exists();
 
             // 購買記錄
             $chapter_ids = collect($data['chapters'])->pluck('chapter_id')->toArray();
@@ -75,22 +76,8 @@ class BookController extends BaseController
             }
         }
 
-
-        VisitBook::dispatch($id, $user);
-        // $book = Book::find($id);
-
-        // Book::withoutEvents(function () use ($book) {
-            // 訪問數+1
-            // $book->increment('view_counts');
-
-            // 添加到排行榜
-            // $book->logRanking();
-        // });
-
-        // 記錄用戶訪問
-        // if ($user) {
-        //     $request->user()->visit($book);
-        // }
+        // 排程: 訪問數+1 / 更新漫畫排行榜 / 記錄用戶訪問
+        VisitBook::dispatch($book_id, $user);
 
         return Response::jsonSuccess(__('api.success'), $data);
     }
@@ -99,65 +86,51 @@ class BookController extends BaseController
     {
         $cache_key = 'chapter:' . $chapter_id;
 
-        $chapter = Cache::remember($cache_key, 28800, function () use ($chapter_id) {
-            return BookChapter::with(['purchase_log'])->find($chapter_id);
+        $data = Cache::remember($cache_key, 20, function () use ($chapter_id) {
+            try {
+                $chapter = BookChapter::with(['purchase_log'])->findOrFail($chapter_id);
+
+                return [
+                    'protect' => true, // 收費章節
+                    'chapter' => json_decode((new BookChapterResource($chapter))->purchased(false)->toJson(), true),
+                    'content' => $chapter->content,
+                    'prev_chapter_id' => $chapter->prev_chapter_id,
+                    'next_chapter_id' => $chapter->next_chapter_id,
+                ];
+            } catch (\Exception $e) {
+                throw new HttpResponseException(Response::jsonError('该漫画不存在或已下架！'));
+            }
         });
-
-        // $chapter = BookChapter::with(['purchase_log'])->find($chapter_id);
-
-        if (!$chapter) {
-            return Response::jsonError('该漫画不存在或已下架！');
-        }
-
-        BookChapter::withoutEvents(function () use ($chapter) {
-            // 訪問數+1
-            $chapter->increment('view_counts');
-        });
-
-        // 收費章節
-        $protect = true;
-
+        
         // 如果章節免費
-        if ($chapter->price == 0) {
-            $protect = false;
+        if ($data['chapter']['price'] == 0) {
+            $data['protect'] = false;
         }
 
         // 是否購買
         $user = $request->user() ?? null;
 
-        $purchased  = false;
-
         if ($user) {
             if ($user->is_vip) {
-                $purchased  = true;
+                $purchased = true;
             } else {
-                $purchased = $user->purchase_logs()->where('type', 'book_chapter')->where('item_id', $chapter->id)->exists();
+                $purchased = $user->purchase_logs()->where('type', 'book_chapter')->where('item_id', $data['chapter']['chapter_id'])->exists();
+            }
+
+            $data['chapter']['purchased'] = $purchased;
+
+            if ($purchased) {
+                $data['protect'] = false;
             }
         }
 
-        if ($purchased) {
-            $protect = false;
+        if ($data['protect']) {
+            // 如果是收費章節, 只返回第一張圖
+            $data['content'] = [$data['content'][0]];
         }
 
-        $images = $chapter->content;
-
-        if ($protect) {
-            $images = [$images[0]];
-        }
-
-        // $images = collect($images)->map(function ($image) {
-        //     $file = file_get_contents($image);
-        //     $base64 = 'data:image/jpg;base64,' . base64_encode($file);
-        //     return [$base64];
-        // })->toArray();
-
-        $data = [
-            'protect' => $protect,
-            'chapter' => (new BookChapterResource($chapter))->purchased($purchased),
-            'content' => $images,
-            'prev_chapter_id' => $chapter->prev_chapter_id,
-            'next_chapter_id' => $chapter->next_chapter_id,
-        ];
+        // 排程: 訪問數+1
+        VisitBookChapter::dispatch($chapter_id);
 
         return Response::jsonSuccess(__('api.success'), $data);
     }
