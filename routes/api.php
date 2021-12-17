9c3f4fb5c36c6eee4ed4852ccfa643866aae92e6
RB <xtheme@gmail.com>
2021-12-17 15:35:21 +0800

    refactor: clear api route

--- routes/api.php
+++ routes/api.php
@@ -14,139 +14,145 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::as('api.')->middleware(['api'])->group(function () {
+Route::middleware(['api'])->group(function () {
 
     Route::get('/stress', [Api\DevController::class, 'stress']);
     Route::get('/decrypt', [Api\DevController::class, 'decrypt']);
 
     // 第三方支付回調
-    Route::prefix('payment')->as('payment.')->group(function () {
-        // Route::any('/callback', [Api\PaymentController::class, 'callback'])->name('callback'); // 支付結果回調
-        Route::any('/notify/{order_no?}', [Api\PaymentController::class, 'notify'])->name('notify'); // 支付結果回調
+    Route::prefix('payment')->group(function () {
+        // Route::any('/callback', [Api\PaymentController::class, 'callback']); // 支付結果回調
+        Route::any('/notify/{order_no?}', [Api\PaymentController::class, 'notify']);
         Route::any('/mockCallback', [Api\PaymentController::class, 'mockCallback']); // 測試接口:支付結果回調
     });
 
     Route::prefix(config('api.version'))->middleware(['request.decrypt', 'api.location', 'api.header', 'api.sign'])->group(function () {
-        Route::prefix('bootstrap')->as('bootstrap.')->group(function () {
-            Route::get('/configs', [Api\BootstrapController::class, 'configs'])->name('configs');
-            Route::get('/notices', [Api\BootstrapController::class, 'notices'])->name('notices');
+        Route::prefix('bootstrap')->group(function () {
+            Route::get('/configs', [Api\BootstrapController::class, 'configs']);
+            Route::get('/notices', [Api\BootstrapController::class, 'notices']);
         });
 
-        Route::prefix('auth')->as('auth.')->group(function () {
-            Route::post('/captcha', [Api\AuthController::class, 'captcha'])->name('captcha');
-            Route::post('/login', [Api\AuthController::class, 'login'])->name('login');
-            Route::post('/register', [Api\AuthController::class, 'register'])->name('register');
+        Route::prefix('auth')->group(function () {
+            Route::post('/captcha', [Api\AuthController::class, 'captcha']);
+            Route::post('/login', [Api\AuthController::class, 'login']);
+            Route::post('/register', [Api\AuthController::class, 'register']);
             Route::post('/fast/register', [Api\AuthController::class, 'fastRegister']);
         });
 
         // 首頁:導航列
-        Route::prefix('navigation')->as('navigation.')->group(function () {
-            Route::get('/', [Api\NavigationController::class , 'list'])->name('list');
+        Route::prefix('navigation')->group(function () {
+            Route::get('/', [Api\NavigationController::class , 'list']);
         });
 
         // 首頁:主题
-        Route::prefix('topic')->as('topic.')->group(function () {
-            Route::get('/{type}', [Api\TopicController::class, 'list'])->name('list');
-            Route::get('/filter/{filter_id}/{page?}', [Api\TopicController::class, 'filter'])->name('filter');
+        Route::prefix('topic')->group(function () {
+            Route::get('/{type}', [Api\TopicController::class, 'list']);
+            Route::get('/filter/{filter_id}/{page?}', [Api\TopicController::class, 'filter']);
         });
 
         // 視頻
-        Route::prefix('movie')->as('movie.')->group(function () {
-            Route::get('/list/{type}', [Api\MovieController::class, 'list'])->name('list'); // 最新 / 熱門 / (隨機)推薦
-            Route::get('/detail/{id}', [Api\MovieController::class, 'detail'])->name('detail');
+        Route::prefix('movie')->group(function () {
+            Route::get('/list/{type}', [Api\MovieController::class, 'list']);
+            Route::get('/detail/{id}', [Api\MovieController::class, 'detail']);
         });
 
         // 樓鳳履歷
-        Route::prefix('resume')->as('resume.')->group(function () {
-            Route::get('/cities', [Api\ResumeController::class, 'cities'])->name('cities');
-            Route::get('/list/{city?}', [Api\ResumeController::class, 'list'])->name('list');
-            Route::get('/detail/{id}', [Api\ResumeController::class, 'detail'])->name('detail');
+        Route::prefix('resume')->group(function () {
+            Route::get('/cities', [Api\ResumeController::class, 'cities']);
+            Route::get('/list/{city?}', [Api\ResumeController::class, 'list']);
+            Route::get('/detail/{id}', [Api\ResumeController::class, 'detail']);
         });
 
         // 分類頁
-        Route::prefix('category')->as('category.')->group(function () {
-            Route::get('/tags/{type?}', [Api\CategoryController::class, 'tags'])->name('tags');
-            Route::post('/search', [Api\CategoryController::class, 'search'])->name('search');
+        Route::prefix('category')->group(function () {
+            Route::get('/tags/{type?}', [Api\CategoryController::class, 'tags']);
+            Route::post('/search', [Api\CategoryController::class, 'search']);
         });
 
         // 漫畫
-        Route::prefix('book')->as('book.')->group(function () {
-            Route::get('/{id}', [Api\BookController::class, 'detail'])->name('detail');
-            Route::get('/chapter/{chapter_id}', [Api\BookController::class, 'chapter'])->name('chapter');
-            Route::post('/report/{type_id}/{id}', [Api\ReportController::class, 'report'])->name('report');
+        Route::prefix('book')->group(function () {
+            Route::get('/{id}', [Api\BookController::class, 'detail']);
+            Route::get('/chapter/{chapter_id}', [Api\BookController::class, 'chapter']);
+            Route::post('/report/{type_id}/{id}', [Api\ReportController::class, 'report']);
         });
 
         // 排行榜
-        Route::prefix('ranking')->as('ranking.')->group(function () {
-            Route::get('/day/{type?}', [Api\RankingController::class, 'day'])->name('day');
-            Route::get('/week/{type?}', [Api\RankingController::class, 'week'])->name('week');
-            Route::get('/month/{type?}', [Api\RankingController::class, 'month'])->name('month');
-            Route::get('/year/{type?}', [Api\RankingController::class, 'year'])->name('year');
-            Route::get('/japan/{type?}', [Api\RankingController::class, 'japan'])->name('japan');
-            Route::get('/korea/{type?}', [Api\RankingController::class, 'korea'])->name('korea');
-            Route::get('/latest/{type?}', [Api\RankingController::class, 'latest'])->name('latest');
+        Route::prefix('ranking')->group(function () {
+            Route::get('/day/{type?}', [Api\RankingController::class, 'day']);
+            Route::get('/week/{type?}', [Api\RankingController::class, 'week']);
+            Route::get('/month/{type?}', [Api\RankingController::class, 'month']);
+            Route::get('/year/{type?}', [Api\RankingController::class, 'year']);
+            Route::get('/japan/{type?}', [Api\RankingController::class, 'japan']);
+            Route::get('/korea/{type?}', [Api\RankingController::class, 'korea']);
+            Route::get('/latest/{type?}', [Api\RankingController::class, 'latest']);
         });
 
         // 客服
-        Route::prefix('service')->as('service.')->group(function () {
-            Route::get('/url', [Api\ServiceController::class, 'url'])->name('url');
+        Route::prefix('service')->group(function () {
+            Route::get('/url', [Api\ServiceController::class, 'url']);
         });
 
         // 广告 (避開 ad 字眼避免被封鎖)
-        Route::prefix('notable')->as('notable.')->group(function () {
-            Route::get('/space/{id}', [Api\AdController::class, 'space'])->name('space');
+        Route::prefix('notable')->group(function () {
+            Route::get('/space/{id}', [Api\AdController::class, 'space']);
         });
 
         // 安裝 APK/PWA
         Route::prefix('install')->group(function () {
             Route::get('pwa', [Api\InstallController::class, 'pwa']);
         });
+
+        // 用戶反饋
+        Route::prefix('feedback')->group(function () {
+            Route::get('/', [Api\FeedbackController::class, 'questionnaire']);
+            Route::post('/add', [Api\FeedbackController::class, 'add']);
+        });
     });
 
     // 需要 Bearer Token (sanctum 簽發)
     Route::prefix(config('api.version'))->middleware(['request.decrypt', 'api.header', 'api.sign', 'auth:sanctum'])->group(function () {
         // 用戶驗證
-        Route::prefix('auth')->as('auth.')->group(function () {
-            Route::any('/profile', [Api\AuthController::class, 'profile'])->name('profile');
-            Route::any('/logout', [Api\AuthController::class, 'logout'])->name('logout');
-            Route::any('/refresh', [Api\AuthController::class, 'refresh'])->name('refresh');
-            Route::post('/modify', [Api\AuthController::class, 'modify'])->name('modify');
+        Route::prefix('auth')->group(function () {
+            Route::any('/profile', [Api\AuthController::class, 'profile']);
+            Route::any('/logout', [Api\AuthController::class, 'logout']);
+            Route::any('/refresh', [Api\AuthController::class, 'refresh']);
+            Route::post('/modify', [Api\AuthController::class, 'modify']);
         });
 
         // 用戶交易紀錄
-        Route::prefix('user')->as('user.')->group(function () {
-            Route::post('/order', [Api\UserController::class, 'order'])->name('order');
-            Route::post('/recharge', [Api\UserController::class, 'recharge'])->name('recharge');
-            Route::post('/purchase', [Api\UserController::class, 'purchase'])->name('purchase');
+        Route::prefix('user')->group(function () {
+            Route::post('/order', [Api\UserController::class, 'order']);
+            Route::post('/recharge', [Api\UserController::class, 'recharge']);
+            Route::post('/purchase', [Api\UserController::class, 'purchase']);
         });
 
         // 用戶訪問紀錄
-        Route::prefix('visit')->as('visit.')->group(function () {
-            Route::post('/list', [Api\VisitController::class, 'list'])->name('list');
-            Route::post('/destroy', [Api\VisitController::class, 'destroy'])->name('destroy');
+        Route::prefix('visit')->group(function () {
+            Route::post('/list', [Api\VisitController::class, 'list']);
+            Route::post('/destroy', [Api\VisitController::class, 'destroy']);
         });
 
         // 用戶收藏紀錄
-        Route::prefix('favorite')->as('favorite.')->group(function () {
-            Route::post('/list', [Api\FavoriteController::class, 'list'])->name('list');
-            Route::post('/save', [Api\FavoriteController::class, 'save'])->name('save');
-            Route::post('/destroy', [Api\FavoriteController::class, 'destroy'])->name('destroy');
+        Route::prefix('favorite')->group(function () {
+            Route::post('/list', [Api\FavoriteController::class, 'list']);
+            Route::post('/save', [Api\FavoriteController::class, 'save']);
+            Route::post('/destroy', [Api\FavoriteController::class, 'destroy']);
         });
 
         // 支付中心
-        Route::prefix('payment')->as('payment.')->group(function () {
-            Route::get('/pricing', [Api\PaymentController::class, 'pricing'])->name('pricing'); // 支付方案
-            Route::get('/gateway/{pricing_id}', [Api\PaymentController::class, 'gateway'])->name('gateway'); // 支付渠道
-            Route::post('/pay', [Api\PaymentController::class, 'pay'])->name('pay'); // 調用渠道支付
+        Route::prefix('payment')->group(function () {
+            Route::get('/pricing', [Api\PaymentController::class, 'pricing']); // 支付方案
+            Route::get('/gateway/{pricing_id}', [Api\PaymentController::class, 'gateway']); // 支付渠道
+            Route::post('/pay', [Api\PaymentController::class, 'pay']); // 調用渠道支付
         });
 
         // 購買商品
-        Route::post('/purchase', [Api\PurchaseController::class, 'purchase'])->name('purchase');
+        Route::post('/purchase', [Api\PurchaseController::class, 'purchase']);
 
         // 用戶反饋
-        Route::prefix('feedback')->as('feedback.')->group(function () {
-            Route::get('/questionnaire', [Api\FeedbackController::class, 'questionnaire']);
-        });
+        // Route::prefix('feedback')->group(function () {
+        //     Route::get('/', [Api\FeedbackController::class, 'questionnaire']);
+        // });
     });
 });
 
@@ -160,24 +166,24 @@ Route::fallback(function () {
 
 /*
 // 簡訊
-Route::prefix('sms')->as('sms.')->group(function () {
-    Route::post('/verify', [Api\SmsController::class, 'verify'])->name('verify'); // 校验SSO
-    Route::post('/send', [Api\SmsController::class, 'send'])->name('send');
+Route::prefix('sms')->group(function () {
+    Route::post('/verify', [Api\SmsController::class, 'verify'])O
+    Route::post('/send', [Api\SmsController::class, 'send']);
 });
 
 // 动画
-Route::prefix('video')->as('video.')->group(function () {
-    Route::get('/list/{page?}', [Api\VideoController::class, 'list'])->name('list');
-    Route::get('/detail/{id}', [Api\VideoController::class, 'detail'])->name('detail');
-    Route::get('/recommend/{id?}', [Api\VideoController::class, 'recommend'])->name('recommend');
-    Route::post('/play/{id}/{series_id}', [Api\VideoController::class, 'play'])->name('play');
+Route::prefix('video')->group(function () {
+    Route::get('/list/{page?}', [Api\VideoController::class, 'list']);
+    Route::get('/detail/{id}', [Api\VideoController::class, 'detail']);
+    Route::get('/recommend/{id?}', [Api\VideoController::class, 'recommend']);
+    Route::post('/play/{id}/{series_id}', [Api\VideoController::class, 'play']);
 });
 
 // 评论
-Route::prefix('comment')->as('comment.')->group(function () {
-    Route::get('/list/{chapter_id}/{order}', [Api\CommentController::class, 'list'])->name('list');
-    Route::post('/add', [Api\CommentController::class, 'add'])->name('add');
-    Route::post('/like/{comment_id}', [Api\CommentController::class, 'like'])->name('like');
-    Route::post('/destroy/{comment_id}', [Api\CommentController::class, 'destroy'])->name('destroy');
+Route::prefix('comment')->group(function () {
+    Route::get('/list/{chapter_id}/{order}', [Api\CommentController::class, 'list']);
+    Route::post('/add', [Api\CommentController::class, 'add']);
+    Route::post('/like/{comment_id}', [Api\CommentController::class, 'like']);
+    Route::post('/destroy/{comment_id}', [Api\CommentController::class, 'destroy']);
 });
 */
