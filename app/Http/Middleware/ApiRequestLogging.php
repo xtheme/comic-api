5de02bfd4c8ea129fc92201dab7aa52cb5384a67
RB <xtheme@gmail.com>
2022-03-09 16:49:32 +0800

    refactor: ApiRequestLogging

--- app/Http/Middleware/ApiRequestLogging.php
+++ app/Http/Middleware/ApiRequestLogging.php
@@ -5,38 +5,32 @@ namespace App\Http\Middleware;
 use App\Models\ApiRequestLog;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class ApiRequestLogging
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  Request  $request
+     * @param  Closure  $next
      *
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
-        // if (defined('LARAVEL_START')) {
-        //     $time = $this->getElapsedTimeInMs();
-        //     Log::debug(sprintf('IP: %s, Api: %s (Prepare: %s sec)', $request->ip(), $request->fullUrl(), $time));
-        // }
-
         return $next($request);
     }
 
     /**
      * Handle tasks after the response has been sent to the browser.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      *
      * @return void
      */
     public function terminate(Request $request)
     {
-        if (defined('LARAVEL_START')) {
+        if (config('app.debug') && defined('LARAVEL_START')) {
             $log = new ApiRequestLog;
             $log->user_id = $request->user() ? $request->user()->id : null;
             $log->fingerprint = $request->header('uuid') ?? null;
@@ -49,7 +43,7 @@ class ApiRequestLogging
         }
     }
 
-    protected function getElapsedTimeInMs()
+    protected function getElapsedTimeInMs(): float
     {
         $time = (microtime(true) - LARAVEL_START);
         return round($time, 4);
